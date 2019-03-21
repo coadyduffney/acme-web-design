e9cb24eea75b814de5486915b6312a46211d133b
coadyduffney <coadyduffney@gmail.com>
2019-03-21 10:31:48 -0300

    updated responsive nav function name

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+function responsiveNav() {
   var x = document.getElementById('nav');
   if (x.className === 'navbar') {
     x.className += ' responsive';
